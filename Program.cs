3fe1c689e31d236f2a2d2eaa42ea1f0d0dc07517
Marcin Dziedzic <marcin.dziedzic49@microsoft.wsei.edu.pl>
2022-04-23 13:05:05 +0200

    jest postep

--- Program.cs
+++ Program.cs
@@ -10,9 +10,15 @@ namespace Lab_04
             var fil1 = new DirectoryInfo(path);
             Console.WriteLine(fil1.ToString());
             var xd = fil1.EnumerateDirectories();
+            var wtf = fil1.GetFiles();
+            foreach (var item in wtf)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("reeee");
             foreach (var item in xd)
             {
-                Console.WriteLine(item.Name+ item.mes);
+                Console.WriteLine(item.Name+item.GetFiles().Length);
             }
 
 
