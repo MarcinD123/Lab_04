60b878f118ae9b3b47c3b20947957382e3e3cd32
Marcin Dziedzic <marcin.dziedzic49@microsoft.wsei.edu.pl>
2022-04-23 12:56:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace Lab_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Program Files\dotnet";
+            var fil1 = new DirectoryInfo(path);
+            Console.WriteLine(fil1.ToString());
+            var xd = fil1.EnumerateDirectories();
+            foreach (var item in xd)
+            {
+                Console.WriteLine(item.Name+ item.mes);
+            }
+
+
+
+
+        }
+    }
+}
